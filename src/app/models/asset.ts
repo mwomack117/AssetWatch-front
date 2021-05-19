130d6fc164a83f5e69f1f4eee7143470391d4450
Mike Womack <michael.womack84@gmail.com>
2021-05-19 13:39:31 -0700

    add dashboard, search, portfolio, newsfeed components

---
+++ src/app/models/asset.ts
@@ -0,0 +1,6 @@
+export interface Asset {
+    symbol: string,
+    name: string,
+    open: number,
+    close: number,
+}
---
+++ src/app/models/news-item.ts
@@ -0,0 +1,10 @@
+export interface NewsItem {
+    news_url: string,
+    image_url: string,
+    title: string,
+    text: string,
+    source_name: string,
+    date: string,
+    sentiment: string,
+    tickers: string[],
+}
