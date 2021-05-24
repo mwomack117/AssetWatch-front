836367a470ac6cd64513b983b6b78b56ba8e1c47
Wisterdy Alcida <wisterdyalcida@Wisterdys-MacBook-Air.local>
2021-05-23 21:50:30 -0400

    really hope this work

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,16 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  isLoggedIn: boolean = localStorage.getItem("SessionUser") !== null ? true : false;
-  isLoggedOut: boolean = localStorage.getItem("SessionUser") == null ? true : false;
-
+  /*isLoggedIn: boolean = localStorage.getItem("SessionUser") !== null ? true : false;
+  isLoggedOut: boolean = localStorage.getItem("SessionUser") == null ? true : false;*/
+  isLoggedIn:boolean;
+  isLoggedOut:boolean;
   ngOnInit(): void {
     console.log(localStorage.getItem("SessionUser"));
+    //this.isLoggedIn= false; 
+    //his.isLoggedOut=true;
+
+    if(localStorage.getItem("SessionUser") !== null)
+   {
+          this.isLoggedIn=true;
+          //this.isLoggedOut=false;
+   }
+   if(localStorage.getItem("SessionUser") == null)
+   {
+          this.isLoggedOut=true;
+          //this.isLoggedIn=false;
+   }
+
+  }
+  ngOnchanges(){
+   if(localStorage.getItem("SessionUser") !== null)
+   {
+          this.isLoggedIn=true;
+          this.isLoggedOut=false;
+   }
+   if(localStorage.getItem("SessionUser") == null)
+   {
+          this.isLoggedOut=true;
+          this.isLoggedIn=false;
+   }
 
   }
 
+
   onSignOutClick() {
     localStorage.removeItem("SessionUser");
+
     window.location.reload();
   }
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -15,18 +15,21 @@ export class RegisterComponent implements OnInit {
   Email: string;
   Username: string;
   Password: string;
-  valid: boolean[];
+  fnvalid: boolean;
+  lnvalid:boolean;
+  usernamevalid: boolean;
+  passvalid: boolean;
+  emailvalid:boolean;
+  invalidmessage:string;
+
   constructor(private userservice: UserServiceService) {
-   /*this.valid[0]=false;
-   this.valid[1]=false;
-    this.valid[2]=false;
-    this.valid[3]=false;
-    this.valid[3]=false;*/
+
    
   }
 
   
   ngOnInit(): void {
+    this.invalidmessage="";
   }
 
 
@@ -34,17 +37,75 @@ export class RegisterComponent implements OnInit {
 
   SignupUser() {
    
-/*
-     console.log( "firstname "+ document.forms["myForm"]["firstname"].value);
-     console.log( "lastname "+ document.forms["myForm"]["lastname"].value);
-     console.log( "email"+ document.forms["myForm"]["email"].value);
-     console.log( " password "+ document.forms["myForm"]["password"].value);
-     console.log( "username "+ document.forms["myForm"]["username"].value);
-     console.log(document.forms["myForm"]["username"].valid);
+   this.fnvalid=true;
+   this.lnvalid=true;
+   this.passvalid=true;
+   this.usernamevalid=true;
+   this.emailvalid=true; 
+    let start=0;
+     this.invalidmessage="";
+    //firstname
+    let fname=document.forms["myForm"]["firstname"].value;
+    for(start ; start< 1; start++){
 
-     if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.forms["myForm"]["email"].value))
+    if(fname.length <3)
+      {
+        this.fnvalid=false;
+        this.invalidmessage= "firstname need to be at least three characters";
+        break;
+      }
+    //last name 
+    let lname=document.forms["myForm"]["lastname"].value;
+    if(lname.length <3)
+      {
+        this.lnvalid=false;
+        this.invalidmessage= "lastname need to be at least three letters";
+        break;
+      }
+    //email 
+     this.emailvalid=false;
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.forms["myForm"]["email"].value))
+    {
+
+      this.emailvalid=true;
+      console.log("email is valid");
+      
+           //return true;
+          
+    }
+    if(this.emailvalid==false){
+      this.invalidmessage= "Email is not valid  @email.com";
+      break;
+    }
+    
+    let usname=document.forms["myForm"]["username"].value;
+    if(usname.length <3)
+      {
+        this.usernamevalid=false;
+        this.invalidmessage= "username need to be at least three letters";
+        break;
+      }
+
+
+      let passname=document.forms["myForm"]["password"].value;
+      if(passname.length <3)
+        {
+         this.passvalid=false;
+         this.invalidmessage= "password need to be at least three letters";
+         break;
+        }
+  
+      }
+     
+
+        if(start==1)
+      this.userservice.CreateUser(new UserModel(this.Firstname, this.Lastname, this.Username, this.Password, this.Email))
+
+    /* if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.forms["myForm"]["email"].value))
      {
+      console.log("email is valid");
             return true;
+           
      }*/
     /*console.log("hello Firstname " + this.Firstname);
     console.log("hello lastname " + this.Lastname);
